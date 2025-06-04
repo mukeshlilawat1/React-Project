a205f643b7768ee8e87d030aacde454bea067a08
mukeshlilawat1 <mukeshkumarlilawat1@gmail.com>
2025-06-05 00:17:27 +0530

    updated

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/Header'
 import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <Router >
+        <Header/>
         <Switch>
           <Route exact path='/'>
             <Login />
