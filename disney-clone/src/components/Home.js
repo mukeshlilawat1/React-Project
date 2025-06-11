a4650af73c26868151b7c6ea93817ed1da4cdef3
mukeshlilawat1 <mukeshkumarlilawat1@gmail.com>
2025-06-11 11:39:18 +0530

    update

--- disney-clone/src/components/Home.js
+++ disney-clone/src/components/Home.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider';
-import Viewers from './Viewers';
+import Viewers from './viewers';
+import Recommands from './Recommends';
+import NewDisney from './NewDisney';
+import Sports from './Sports';
+import Originals from './Originals';
 
 
 
@@ -8,25 +12,31 @@ const Home = (props) => {
     return (<Container>
         <ImgSlider />
         <Viewers />
+        <Recommands />
+        <NewDisney />
+        <Sports />
+        <Originals />
     </Container>
     );
 };
 
 const Container = styled.main`
-position : relative;
-min-height : calc(100vh - 250px)
-overflow-x : hidden;
-display : block;
-top : 72px;
-padding : 0 calc(3.5vw + 5px);
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
 
-&:after {
-background : url('/images/home-background.png') center center / cover no-repeat fixed;
-content: "";
-position: absolute;
-inset: 0px;
-opacity : 1;
-z-index : -1;
-}
+  &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
 `;
+
 export default Home;
